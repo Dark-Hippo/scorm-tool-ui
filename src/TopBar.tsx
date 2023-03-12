7da38121d3f52e01ff5996d11f41abf3a46e6e40
Tom Odell <Dark-Hippo@users.noreply.github.com>
2023-03-12 18:27:50 +0000

    Add additional styling for nav bar

--- src/TopBar.tsx
+++ src/TopBar.tsx
@@ -4,7 +4,7 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { Button } from '@mui/material';
 import './TopBar.css';
 
@@ -13,15 +13,12 @@ export default function TopBar() {
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h6" component="div" sx={{ marginRight: 2 }}>
-            Scorm Tool v1.0
-          </Typography>
           <Box sx={{ display: { md: 'flex' } }}>
             <Button sx={{}}>
-              <Link to="/">Home</Link>
+              <NavLink to="/">Home</NavLink>
             </Button>
             <Button sx={{}}>
-              <Link to="/upload">Upload</Link>
+              <NavLink to="/upload">Upload</NavLink>
             </Button>
           </Box>
         </Toolbar>
