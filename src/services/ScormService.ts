e0e306009126a2d7c4babd460a5cdb8e5395fbad
Tom Odell <Dark-Hippo@users.noreply.github.com>
2023-05-12 14:45:50 +0100

    Add UploadScormFile service

--- src/services/ScormService.ts
+++ src/services/ScormService.ts
@@ -1,8 +1,13 @@
-import { SERVER } from '../config';
+import { SERVER, USER_ID } from '../config';
 
-import type { ScormValidateResponse } from '../types/Responses';
+import type {
+  ScormValidateResponse,
+  ScormUploadResponse,
+} from '../types/Responses';
 
-export const ValidateScormFile = async (file: File) => {
+export const ValidateScormFile = async (
+  file: File
+): Promise<ScormValidateResponse> => {
   const formData = new FormData();
   formData.append('scorm', file);
   const response = await fetch(`${SERVER}/scorm/validate`, {
@@ -11,6 +16,22 @@ export const ValidateScormFile = async (file: File) => {
     mode: 'cors',
   });
   const result: ScormValidateResponse = await response.json();
+
+  return result;
+};
+
+export const UploadScormFile = async (
+  file: File
+): Promise<ScormUploadResponse> => {
+  const formData = new FormData();
+  formData.append('scorm', file);
+  formData.append('userId', USER_ID.toString());
+  const response = await fetch(`${SERVER}/scorm/upload`, {
+    method: 'POST',
+    body: formData,
+    mode: 'cors',
+  });
+  const result: ScormUploadResponse = await response.json();
 
   return result;
 };
