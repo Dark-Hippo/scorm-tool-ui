a89c2a48896bf57a60b745ee878ed9dc3eb7294d
Tom Odell <Dark-Hippo@users.noreply.github.com>
2023-05-12 15:38:39 +0100

    Update courses page to return courses

--- src/pages/Courses.tsx
+++ src/pages/Courses.tsx
@@ -1,51 +1,23 @@
 import { useEffect, useState } from 'react';
-import { SERVER } from '../config';
 
 import type { Course } from '../types/Course';
+import { ListCourses } from '../services/CourseService';
+import { CourseList } from '../components/CourseList';
 
 export default function Courses() {
   const [loading, setLoading] = useState(false);
-  const [courses, setCourses] = useState<Course[]>();
+  const [courses, setCourses] = useState<Course[]>([]);
 
   if (loading) return <h1>Loading.</h1>;
 
   useEffect(() => {
     async function getCourses() {
-      const response = await fetch(`${SERVER}/course`, {
-        method: 'GET',
-        mode: 'cors',
-      });
-      const result: Course[] = await response.json();
-      console.log(result);
-      setCourses(result);
+      const courses = await ListCourses();
+      setCourses(courses);
     }
 
     getCourses();
   }, []);
 
-  // const SendToServer = async (file: File) => {
-  //   const formData = new FormData();
-  //   formData.append('scorm', file);
-  //   const response = await fetch(`${SERVER}/scorm`, {
-  //     method: 'POST',
-  //     body: formData,
-  //     mode: 'cors',
-  //   });
-  //   const result = await response.json();
-
-  //   return result;
-  // };
-
-  const courseList = courses?.map((course) => (
-    <li>
-      {course.id}.{course.name} ({course.language}) -{' '}
-      <em>{new Date(course.updatedDate).toLocaleDateString()}</em>
-    </li>
-  ));
-
-  return (
-    <div>
-      <ul>{courseList}</ul>
-    </div>
-  );
+  return <CourseList data={courses} />;
 }
