fbb26a0d9e3f8efdee71c7cfec32591e417162a2
Tom Odell <Dark-Hippo@users.noreply.github.com>
2023-05-14 12:30:31 +0100

    Remove demo data and update return type

--- src/pages/Upload.tsx
+++ src/pages/Upload.tsx
@@ -9,35 +9,8 @@ import { FileWithPath, FileRejection } from 'react-dropzone';
 import type { CourseWithSite } from '../types/CourseWithSite';
 import { UploadScormFile } from '../services/ScormService';
 
-const courses: CourseWithSite[] = [
-  {
-    courseId: 3,
-    siteId: 3,
-    guid: '',
-    name: 'Test course',
-    language: 'gb',
-    title: '',
-  },
-  {
-    courseId: 4,
-    siteId: 4,
-    guid: '',
-    name: 'Test course 2',
-    language: 'gb',
-    title: 'something.zip',
-  },
-  {
-    courseId: 5,
-    siteId: 5,
-    guid: '',
-    name: 'Test course 3',
-    language: 'fr',
-    title: 'something-else.zip',
-  },
-];
-
 export const Upload = () => {
-  const [courseList, setCourseList] = useState<CourseWithSite[]>(courses);
+  const [courseList, setCourseList] = useState<CourseWithSite[]>([]);
   const [invalidFileList, setInvalidFileList] = useState<FileWithStatus[]>([]);
 
   const onDrop = useCallback(
@@ -65,14 +38,7 @@ export const Upload = () => {
           return;
         }
 
-        const newCourseWithSite: CourseWithSite = {
-          courseId: uploadResponse.course.id,
-          siteId: uploadResponse.site.id,
-          guid: uploadResponse.site.guid,
-          name: uploadResponse.course.name,
-          language: uploadResponse.course.language,
-          title: uploadResponse.site.title,
-        };
+        const newCourseWithSite: CourseWithSite = uploadResponse.course;
         setCourseList([...courseList, newCourseWithSite]);
       });
 
