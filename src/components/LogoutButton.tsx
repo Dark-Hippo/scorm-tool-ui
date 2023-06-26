f23819c6ba0799bcd07226c579d7f750bd3f01a5
Tom Odell <Dark-Hippo@users.noreply.github.com>
2023-06-26 10:09:00 +0100

    Add icon to login button

--- src/components/LogoutButton.tsx
+++ src/components/LogoutButton.tsx
@@ -1,5 +1,6 @@
 import { useAuth0 } from '@auth0/auth0-react';
 import { Button } from '@mui/material';
+import { Logout } from '@mui/icons-material';
 
 export const LogoutButton = () => {
   const { logout } = useAuth0();
@@ -14,6 +15,7 @@ export const LogoutButton = () => {
       }
     >
       Log Out
+      <Logout style={{ marginLeft: '5px' }} />
     </Button>
   );
 };
