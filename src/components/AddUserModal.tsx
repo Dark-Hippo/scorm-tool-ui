bdc47cd7f282ac917e73f56df686ca9c55fe2af0
Tom Odell <Dark-Hippo@users.noreply.github.com>
2023-08-30 18:47:03 +0100

    Add AddUserModal component

---
+++ src/components/AddUserModal.tsx
@@ -0,0 +1,65 @@
+import { useState } from 'react';
+import { Button, Box, Modal } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import { useAuth0 } from '@auth0/auth0-react';
+import AddUser from './AddUser';
+import { NewUserData } from '../types/UserProfile';
+import { createUser } from '../services/UserService';
+
+const Form = styled('form')(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+}));
+
+const SubmitButton = styled(Button)(({ theme }) => ({}));
+
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
+export default function AddUserModal() {
+  const { getAccessTokenSilently } = useAuth0();
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setModalOpen(false);
+  };
+
+  const handleSubmit = async (userData: NewUserData) => {
+    const token = await getAccessTokenSilently();
+    await createUser(userData, token);
+    handleCloseModal();
+  };
+
+  return (
+    <Form>
+      <SubmitButton
+        type="button"
+        variant="contained"
+        color="primary"
+        onClick={handleOpenModal}
+      >
+        Add User
+      </SubmitButton>
+      <Modal open={modalOpen} onClose={handleCloseModal}>
+        <Box sx={style}>
+          <h2>Add New User</h2>
+          <AddUser onSubmit={handleSubmit} />
+        </Box>
+      </Modal>
+    </Form>
+  );
+}
