be1b096f116010ef20802b0644931c63cb5275b3
Tom Odell <Dark-Hippo@users.noreply.github.com>
2023-08-30 18:46:50 +0100

    Add AddUser component

---
+++ src/components/AddUser.tsx
@@ -0,0 +1,67 @@
+import { useState } from 'react';
+import { Button, TextField } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import { NewUserData } from '../types/UserProfile';
+
+const Form = styled('form')(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+}));
+
+const Input = styled(TextField)(({ theme }) => ({
+  marginBottom: theme.spacing(2),
+}));
+
+const SubmitButton = styled(Button)(({ theme }) => ({
+  marginTop: theme.spacing(2),
+}));
+
+interface AddUserFormProps {
+  onSubmit: (userData: NewUserData) => Promise<void>;
+}
+
+export default function AddUserForm({ onSubmit }: AddUserFormProps) {
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+
+  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+
+  const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
+  };
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const userData = { email, name };
+    await onSubmit(userData);
+    setEmail('');
+    setName('');
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Input
+        label="Email"
+        variant="outlined"
+        size="small"
+        value={email}
+        onChange={handleEmailChange}
+        required
+      />
+      <Input
+        label="Name"
+        variant="outlined"
+        size="small"
+        value={name}
+        onChange={handleNameChange}
+        required
+      />
+      <SubmitButton type="submit" variant="contained" color="primary">
+        Add User
+      </SubmitButton>
+    </Form>
+  );
+}
