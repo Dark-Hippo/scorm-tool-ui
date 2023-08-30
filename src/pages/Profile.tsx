ef2e753fbe7746158d7b8564c57a434bbbe9f643
Tom Odell <Dark-Hippo@users.noreply.github.com>
2023-08-30 11:16:07 +0100

    Add basic profile page

---
+++ src/pages/Profile.tsx
@@ -0,0 +1,12 @@
+import UserProfile from '../components/Profile';
+
+function ProfilePage() {
+  return (
+    <div>
+      <h1>Profile</h1>
+      <UserProfile />
+    </div>
+  );
+}
+
+export default ProfilePage;
