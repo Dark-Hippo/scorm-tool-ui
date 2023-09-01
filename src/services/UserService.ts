eb9ca371ee19717192bbed5477dfffa3bb4c76d2
Tom Odell <Dark-Hippo@users.noreply.github.com>
2023-09-01 10:55:34 +0100

    Update userService to make API calls

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -5,91 +5,100 @@ export const createUser = async (
   profileData: UserData,
   accessToken: string
 ): Promise<UserData> => {
-  // const response = await fetch(`/api/profiles`, {
-  //   method: 'POST',
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //     Authorization: `Bearer ${accessToken}`,
-  //   },
-  //   body: JSON.stringify(profileData),
-  // });
+  const response = await fetch(`/api/users`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${accessToken}`,
+    },
+    body: JSON.stringify(profileData),
+  });
 
-  // if (!response.ok) {
-  //   LogError({
-  //     status: response.status,
-  //     message: response.statusText,
-  //   });
-  //   throw new Error('Failed to create profile');
-  // }
+  if (!response.ok) {
+    LogError({
+      status: response.status,
+      message: response.statusText,
+    });
+    throw new Error('Failed to create profile');
+  }
 
-  // Return mock data
-  profileData.id = '123';
   return profileData;
 };
 
 export const getUser = async (
-  email: string,
+  userId: number,
   accessToken: string
 ): Promise<UserData | null> => {
-  // Return mock data
-  return {
-    id: '123',
-    email: email,
-    name: 'John Doe',
-    lastLoggedIn: new Date('2021-08-01T12:34:56Z'),
-  };
+  const response = await fetch(`/api/users/${userId}`, {
+    headers: {
+      'Content-Type': 'application/json',
+
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
+  if (!response.ok) {
+    LogError({
+      status: response.status,
+      message: response.statusText,
+    });
+    throw new Error('Failed to get profile');
+  }
+
+  return response.json();
 };
 
 export const getUsers = async (
   accessToken: string
 ): Promise<UserData[] | null> => {
-  // Return mock data
-  return [
-    {
-      id: '123',
-      email: 'john.doe@example.com',
-      name: 'John Doe',
-      lastLoggedIn: new Date('2021-08-01T12:34:56Z'),
+  const response = await fetch(`/api/users`, {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${accessToken}`,
     },
-    {
-      id: '456',
-      email: 'bob.bobbington@example.com',
-      name: 'Bob Bobbington',
-      lastLoggedIn: new Date('2021-08-01T12:34:56Z'),
-    },
-  ];
+  });
+
+  if (!response.ok) {
+    LogError({
+      status: response.status,
+      message: response.statusText,
+    });
+    throw new Error('Failed to get profiles');
+  }
+
+  return response.json();
 };
 
 export const updateUser = async (
-  email: string,
+  userId: number,
   profileData: UserData,
   accessToken: string
 ): Promise<UserData> => {
-  // const response = await fetch(`/api/profiles/${email}`, {
-  //   method: 'PUT',
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //     Authorization: `Bearer ${accessToken}`,
-  //   },
-  //   body: JSON.stringify(profileData),
-  // });
+  const response = await fetch(`/api/profiles/${userId}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${accessToken}`,
+    },
+    body: JSON.stringify(profileData),
+  });
 
-  // if (!response.ok) {
-  //   LogError({
-  //     status: response.status,
-  //     message: response.statusText,
-  //   });
-  //   throw new Error('Failed to update profile');
-  // }
+  if (!response.ok) {
+    LogError({
+      status: response.status,
+      message: response.statusText,
+    });
+    throw new Error('Failed to update profile');
+  }
 
   return profileData;
 };
 
 export const deleteUser = async (
-  email: string,
+  userId: number,
   accessToken: string
 ): Promise<void> => {
-  const response = await fetch(`/api/profiles/${email}`, {
+  const response = await fetch(`/api/profiles/${userId}`, {
     method: 'DELETE',
     headers: {
       Authorization: `Bearer ${accessToken}`,
