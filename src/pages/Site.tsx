d3814c748cfd03329c68cccbd6e03251796df554
Tom Odell <Dark-Hippo@users.noreply.github.com>
2023-10-17 20:06:12 +0100

    Add iframe element highlighting

--- src/pages/Site.tsx
+++ src/pages/Site.tsx
@@ -1,9 +1,12 @@
 import { Edit } from '@mui/icons-material';
 import { Box, Button } from '@mui/material';
+import { useRef, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { LogError } from '../services/ErrorService';
 
 export const Site = () => {
   const { id, guid } = useParams();
+  const [editing, setEditing] = useState(false);
 
   if (!id) {
     return null;
@@ -13,28 +16,82 @@ export const Site = () => {
     return null;
   }
 
-  const addHoverHighlight = (element: HTMLElement, highlightColor: string) => {
-    element.addEventListener('mouseenter', () => {
-      element.style.outlineColor = highlightColor;
-      element.style.outlineStyle = 'solid';
-      element.style.outlineWidth = '1px';
-    });
+  const iframeRef = useRef<HTMLIFrameElement>(null);
 
-    element.addEventListener('mouseleave', () => {
-      element.style.outline = 'none';
-    });
+  const addOutline = (ev: MouseEvent) => {
+    const element = ev.currentTarget as HTMLElement;
+    if (!element || !element.style) {
+      return;
+    }
+    element.style.outlineColor = '#ff0000';
+    element.style.outlineStyle = 'solid';
+    element.style.outlineWidth = '1px';
+    // element.style.outline = '1px solid #ff0000';
   };
+
+  const removeOutline = (ev: MouseEvent) => {
+    const element = ev.currentTarget as HTMLElement;
+    if (!element || !element.style || !element.style.outline) {
+      return;
+    }
+    element.style.outline = 'unset';
+  };
+
+  const addHoverHighlight = (element: HTMLElement) => {
+    element.addEventListener('mouseenter', addOutline);
+    element.addEventListener('mouseleave', removeOutline);
+  };
+
+  useEffect(() => {
+    const iframe = iframeRef.current;
+    if (!iframe) {
+      LogError({ status: 0, message: 'iframe not found' });
+      return;
+    }
+
+    const iframeWindow = iframe.contentWindow;
+    if (!iframeWindow) {
+      LogError({ status: 0, message: 'iframe contentWindow not found' });
+      return;
+    }
+
+    const childElements = iframeWindow.document.querySelectorAll('*');
+    if (editing) {
+      childElements.forEach((element) => {
+        const el = element as HTMLElement;
+        addHoverHighlight(el);
+      });
+    } else {
+      childElements.forEach((element) => {
+        const el = element as HTMLElement;
+        el.removeEventListener('mouseenter', addOutline);
+        el.removeEventListener('mouseleave', removeOutline);
+        el.style.outline = 'unset';
+      });
+    }
+
+    return () => {
+      childElements.forEach((element) => {
+        const el = element as HTMLElement;
+        el.removeEventListener('mouseenter', addOutline);
+        el.removeEventListener('mouseleave', removeOutline);
+      });
+    };
+  }, [editing]);
 
   return (
     <div style={{ height: '85vh' }}>
       <Box sx={{ textAlign: 'right', height: '5vh' }}>
-        <Button variant="contained">
+        <Button
+          color={editing ? 'error' : 'primary'}
+          variant="contained"
+          onClick={() => setEditing(!editing)}
+        >
           <Edit />
         </Button>
       </Box>
       <iframe
-        onMouseEnter={(e) => addHoverHighlight(e.currentTarget, '#ff0000')}
-        onMouseLeave={(e) => addHoverHighlight(e.currentTarget, '')}
+        ref={iframeRef}
         src={`/api/content/${guid}/course/scormcontent/`}
         width="100%"
         height="100%"
