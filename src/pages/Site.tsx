43ab7d00cacd656a4ab5c5c8d9fd59f3e7b7011a
Tom Odell <Dark-Hippo@users.noreply.github.com>
2023-10-17 18:12:33 +0100

    Add iframe border on hover

--- src/pages/Site.tsx
+++ src/pages/Site.tsx
@@ -1,3 +1,5 @@
+import { Edit } from '@mui/icons-material';
+import { Box, Button } from '@mui/material';
 import { useParams } from 'react-router-dom';
 
 export const Site = () => {
@@ -11,9 +13,28 @@ export const Site = () => {
     return null;
   }
 
+  const addHoverHighlight = (element: HTMLElement, highlightColor: string) => {
+    element.addEventListener('mouseenter', () => {
+      element.style.outlineColor = highlightColor;
+      element.style.outlineStyle = 'solid';
+      element.style.outlineWidth = '1px';
+    });
+
+    element.addEventListener('mouseleave', () => {
+      element.style.outline = 'none';
+    });
+  };
+
   return (
     <div style={{ height: '85vh' }}>
+      <Box sx={{ textAlign: 'right', height: '5vh' }}>
+        <Button variant="contained">
+          <Edit />
+        </Button>
+      </Box>
       <iframe
+        onMouseEnter={(e) => addHoverHighlight(e.currentTarget, '#ff0000')}
+        onMouseLeave={(e) => addHoverHighlight(e.currentTarget, '')}
         src={`/api/content/${guid}/course/scormcontent/`}
         width="100%"
         height="100%"
